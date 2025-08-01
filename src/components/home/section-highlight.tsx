9b73b95bb8334d626c86d9d7e0186b7b5d62533a
Youssifou Awanou <youssifou.awanou@gmail.com>
2025-08-01 18:33:50 +0000

    -setup projet -integration home page

--- src/components/home/section-highlight.tsx
+++ src/components/home/section-highlight.tsx
@@ -15,13 +15,13 @@ export const SectionHighlight: React.FC<SectionHighlightProps> = ({
                                                                   }) => {
     return (
         <div
-            className={`flex-wrap md:flex-nowrap w-full space-x-11 rounded-xl items-center py-[60px] px-6 space-y-6 md:space-y-0 md:py-[120px] md:px-12 ${backgroundClass} bg-gradient-to-b`}
+            className={`md:flex w-full space-x-11 rounded-xl items-center py-[60px] px-6 space-y-6 md:space-y-0 md:py-[120px] md:px-12 ${backgroundClass} bg-gradient-to-b`}
         >
-            <div className="w-full md:w-[450px]">
+            <div className="w-full md:w-1/2">
                 <h3 className="font-semibold text-4xl text-white">{title}</h3>
                 <p className="text-white/60 text-lg mt-5">{description}</p>
             </div>
-            <div>
+            <div className="w-full md:w-1/2">
                 <img
                     className="w-full md:w-[375px]"
                     src={imageSrc}
